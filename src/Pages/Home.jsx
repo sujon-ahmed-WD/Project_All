10ef12edbd5cd4f480f7dd630a3515c86c7dac1d
sujon-ahmed-WD <sujon.mrist@gmail.com>
2024-09-03 23:29:42 +0600

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import Nav from "../Components/Nav";
+
+ 
+
+const Home = () => {
+    return (
+        <div>
+            <Nav/>
+            <h1>This is a Home Pages</h1>
+        </div>
+    );
+};
+
+export default Home;
